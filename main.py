ed77eddd619308cd8fb3b3b24586dcce25eafe96
road-yasu <kaminotani5@gmail.com>
2021-08-09 00:38:12 +0900

    .ignoreファイルを追加 main.py処理を関数化

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-import glob
 import imghdr
 import os
 import pathlib
-import pprint
+import shutil
 
-path_images = pathlib.Path('/Users/strada/Desktop/images')
-files = list(path_images.glob('**/*'))
 
-for file in files:
-  if os.path.isfile(file):
-    if os.path.basename(file)[:1] != '.':
-      imagetype = imghdr.what(file)
-      if imagetype == 'png' or 'jpg':
-        print(file)
+# 拡張子のないファイルを移動させる *argsが拡張子
+def move_file(current_folder, move_folder, *args):
+  num = 1
+  for file in pathlib.Path(current_folder).rglob('**/*'):
+    if os.path.isfile(file):
+      # 拡張子がついていないものだけ取得する
+      if os.path.splitext(file)[1] == '':
+        imagetype = imghdr.what(file)
+        for arg in args:
+          if imagetype == arg:
+            shutil.copy(file, move_folder + file.stem + '_' + str(num))
+            num += 1
+
+# 対象のフォルダ内の拡張子がないファイルを拡張子つきにリネームする
+def rename_file(folder):
+  for file in pathlib.Path(folder).rglob('*'):
+    imagetype = imghdr.what(file)
+    if imagetype is not None:
+      file.rename(folder + file.stem + '.' + imagetype)
+
+if __name__ == '__main__':
+  target_folder = '/Users/strada/Desktop/images'
+  save_folder = '/Users/strada/Desktop/rename/'
+
+  move_file(target_folder, save_folder, 'png', 'jpg','jpeg', 'gif')
+  rename_file(save_folder)
