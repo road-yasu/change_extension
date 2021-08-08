458bfeed0f1fda69f071b742e50c93b2839284a3
road-yasu <kaminotani5@gmail.com>
2021-08-08 12:07:23 +0900

    main.pyを変更

--- main.py
+++ main.py
@@ -4,15 +4,9 @@ import os
 import pathlib
 import pprint
 
-# imagetype = imghdr.what('/Users/strada/Desktop/images/image1')
-# print(imagetype)
-
 path_images = pathlib.Path('/Users/strada/Desktop/images')
-# print(path_images)
 files = list(path_images.glob('**/*'))
 
-# pprint.pprint(list(path_images.glob('**/*')))
-# files = glob.glob('/Users/strada/Desktop/images/**/*')
 for file in files:
   if os.path.isfile(file):
     if os.path.basename(file)[:1] != '.':
